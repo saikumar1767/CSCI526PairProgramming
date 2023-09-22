f194a05c9734c30e719a121f165fc586fe6684ba
Sai Kumar <kasarla@usc.edu>
2023-09-21 23:36:51 -0700

    commit

--- Pair_Program/Assets/Scripts/PlayerController.cs
+++ Pair_Program/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,18 @@ public class PlayerController : MonoBehaviour
     // public float jump;
     private float jumpForce = 10f;
     private bool isGrounded;
-    private float moveSpeed = 3f;
+    private float moveSpeed = 5f;
+    private Transform stick1;
+    private Transform stick2;
+    public Transform destinationPoint;
+
     void Start()
     {
         isGrounded = true;
         rb = GetComponent<Rigidbody2D>();
+
+        stick1 = transform.GetChild(0);
+        stick2 = transform.GetChild(1);
     }
 
     // Update is called once per frame
@@ -32,6 +40,21 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey("n"))
         {
             MoveInAir(Vector2.left);
+        }
+
+        float stick1Rotation = stick1.rotation.eulerAngles.z;
+        float stick2Rotation = stick2.rotation.eulerAngles.z;
+
+        if ((stick1Rotation > 90 && stick1Rotation < 270) || (stick2Rotation > 90 && stick2Rotation < 270))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+
+        float distance = Vector2.Distance(transform.position, destinationPoint.position);
+        
+        if (distance < 13.5f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
